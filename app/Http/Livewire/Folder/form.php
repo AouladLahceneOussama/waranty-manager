4bde684bf020596a2ebb12b48e2da271c0f37dc8
AyatANSSAIEN <ayat.anssaien@etu.uae.ac.ma>
2022-12-18 23:57:13 +0100

    add form first commit

---
+++ app/Http/Livewire/Folder/form.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Livewire\Folder;
+
+use App\Models\Folder;
+use App\Models\Insured;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+class Form extends Component
+{
+    use WithPagination;
+
+    public Folder $folder ;
+    public Insured $primary;
+    public Insured $secondary;
+    public Insured $children;
+    public $insureds =["children" => []];
+
+    public $nom;
+    
+
+    protected $rules = [
+        "folder.compagnie" => "required|string",
+        "folder.souscripteur" => "required|string",
+        "folder.cotisation_ht" => "required|string",
+        "folder.cotisation_ttc" => "required|string",
+        "folder.date_effet" => "required|date",
+        "folder.statut" => "required|string",
+        "insureds.*.nom" =>"required|string",
+        "insureds.*.nom_jeune_fille" =>"required|string",
+        "insureds.*.prenom" =>"required|string",
+        "insureds.*.date_naissance" =>"required|string",
+        "insureds.*.pays_naissance" =>"required|string",
+        "insureds.*.departement_naissance" =>"required|string",
+        "insureds.*.civilite" =>"required|string",
+        "insureds.*.etat_civil" =>"required|string",
+        "insureds.*.code_securite_social" =>"required|string",
+        "insureds.*.primary_phone" =>"required|string",
+        "insureds.*.secondary_phone" =>"required|string",
+        "insureds.*.email" =>"required|string",
+        "insureds.*.iban" =>"required|string",
+        "insureds.*.jours_prelevement" =>"required|string",
+      
+    ];
+
+    public function addChild()
+    {
+        $this->insureds['children'][] = [
+            "nom" => "",
+            "prenom" => "",
+            "date_naissance" => "",
+            "civilite" => "",
+            "code_securite_social" => "",
+        ];
+    }
+    public function removeChild($key)
+    {
+        unset($this->insureds['children'][$key]);
+        $this->insureds['children'] = array_values($this->insureds['children']);
+    }
+
+
+    public function create(){
+        //$this->validate();
+        dd($this->insureds);
+    }
+    public function mount()
+    {
+        $this->folder = new Folder();
+        $this->insured = new Insured();
+        $this->enfant = new Insured();
+    }
+
+    public function render()
+    {
+        return view('folder.form');
+    }
+}
