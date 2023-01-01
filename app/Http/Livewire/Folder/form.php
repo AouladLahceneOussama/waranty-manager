96bbfd13a036537d632d8a5bef44cc8627259687
unknown <oussama@kreatinc.com>
2023-01-01 12:57:12 +0100

    update add folder and insured form

--- app/Http/Livewire/Folder/form.php
+++ app/Http/Livewire/Folder/form.php
@@ -11,14 +11,14 @@ class Form extends Component
 {
     use WithPagination;
 
-    public Folder $folder ;
+    public Folder $folder;
     public Insured $primary;
     public Insured $secondary;
     public Insured $children;
-    public $insureds =["children" => []];
+    public $insureds = ["children" => []];
 
     public $nom;
-    
+
 
     protected $rules = [
         "folder.compagnie" => "required|string",
@@ -27,21 +27,20 @@ class Form extends Component
         "folder.cotisation_ttc" => "required|string",
         "folder.date_effet" => "required|date",
         "folder.statut" => "required|string",
-        "insureds.*.nom" =>"required|string",
-        "insureds.*.nom_jeune_fille" =>"required|string",
-        "insureds.*.prenom" =>"required|string",
-        "insureds.*.date_naissance" =>"required|string",
-        "insureds.*.pays_naissance" =>"required|string",
-        "insureds.*.departement_naissance" =>"required|string",
-        "insureds.*.civilite" =>"required|string",
-        "insureds.*.etat_civil" =>"required|string",
-        "insureds.*.code_securite_social" =>"required|string",
-        "insureds.*.primary_phone" =>"required|string",
-        "insureds.*.secondary_phone" =>"required|string",
-        "insureds.*.email" =>"required|string",
-        "insureds.*.iban" =>"required|string",
-        "insureds.*.jours_prelevement" =>"required|string",
-      
+        "insureds.*.nom" => "required|string",
+        "insureds.*.nom_jeune_fille" => "required|string",
+        "insureds.*.prenom" => "required|string",
+        "insureds.*.date_naissance" => "required|string",
+        "insureds.*.pays_naissance" => "required|string",
+        "insureds.*.departement_naissance" => "required|string",
+        "insureds.*.civilite" => "required|string",
+        "insureds.*.etat_civil" => "required|string",
+        "insureds.*.code_securite_social" => "required|string",
+        "insureds.*.primary_phone" => "required|string",
+        "insureds.*.secondary_phone" => "required|string",
+        "insureds.*.email" => "required|string",
+        "insureds.*.iban" => "required|string",
+        "insureds.*.jours_prelevement" => "required|string",
     ];
 
     public function addChild()
@@ -54,17 +53,19 @@ class Form extends Component
             "code_securite_social" => "",
         ];
     }
+    
     public function removeChild($key)
     {
         unset($this->insureds['children'][$key]);
         $this->insureds['children'] = array_values($this->insureds['children']);
     }
 
-
-    public function create(){
+    public function create()
+    {
         //$this->validate();
         dd($this->insureds);
     }
+
     public function mount()
     {
         $this->folder = new Folder();
