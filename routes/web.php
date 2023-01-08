83b2c7e2c4e9c1bebb265560127fc9a360becd93
unknown <oussama@kreatinc.com>
2023-01-08 22:31:11 +0100

    add auth to / route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    if (Auth::check()) return view('/dashboard');
     return view('auth.login');
 });
 
